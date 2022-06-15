723ad4e3515b5d26c30a29dfcaac6cd7bf7e360e
Tony Goodhew <tony@schnauzergroup.com>
2022-06-15 11:09:40 -0700

    Adding some program control and a second model

--- HPGLTest/Program.cs
+++ HPGLTest/Program.cs
@@ -15,24 +15,61 @@ namespace HPGLTest
             GpibSession gpibSession;
             ResourceManager resManager;
 
+            // Setup the console
+            Output.SetupConsole();
+
+            // Setup the GPIB connection via the ResourceManager
             resManager = new ResourceManager();
 
+            // Connect to the plotter and configure connection
             gpibSession = (GpibSession)resManager.Open(@"GPIB0::6::INSTR");
             gpibSession.TimeoutMilliseconds = 20000; // Set the timeout to be 20s
             gpibSession.TerminationCharacterEnabled = true;
 
+            // Clear the session
             gpibSession.Clear();
 
-            string hpgl = System.IO.File.ReadAllText(@"Shuttle-HPGL.txt");
+            // Start the plotting process
+            Output.Heading("HP 7090A Measurement System Plotter - Samples", true);
+            Output.Information("This program produces two samples - An Imperial Walker in the default color and a colored version of the Space Shuttle");
 
+            // Ensure paper loaded
+            Output.Prompt("Load the paper");
+
+            // Get the first model
+            string hpgl = System.IO.File.ReadAllText(@"ImperialWalker-HPGL.PLT");
+
+            // Split the commands up so they can be sent individually to the plotter
             string[] hpglCommands = Regex.Split(hpgl, @"(?<=[;])");
 
+            // Send the commands
             foreach (var cmd in hpglCommands)
             {
                 gpibSession.FormattedIO.WriteLine(cmd);
 
                 Console.WriteLine(cmd);
             }
+
+            // Flip the paper over
+            Output.Prompt("Flip the paper");
+
+            // Get the second model
+            hpgl = System.IO.File.ReadAllText(@"columbia-Model-SHPGL.PLT");
+
+            // Split the commands up so they can be sent individually to the plotter
+            hpglCommands = Regex.Split(hpgl, @"(?<=[;])");
+
+            // Send the commands
+            foreach (var cmd in hpglCommands)
+            {
+                gpibSession.FormattedIO.WriteLine(cmd);
+
+                Console.WriteLine(cmd);
+            }
+
+            // Plotting complete
+            Output.Prompt("Plotting complete");
+
         }
     }
 }
