f5a1078ccaf1b5063801ffb749c87316a3065b6a
Tony Goodhew <tony@schnauzergroup.com>
2022-06-14 22:05:49 -0700

    Add project files.

---
+++ HPGLTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using NationalInstruments.Visa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HPGLTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GpibSession gpibSession;
+            ResourceManager resManager;
+
+            resManager = new ResourceManager();
+
+            gpibSession = (GpibSession)resManager.Open(@"GPIB0::6::INSTR");
+            gpibSession.TimeoutMilliseconds = 20000; // Set the timeout to be 20s
+            gpibSession.TerminationCharacterEnabled = true;
+
+            gpibSession.Clear();
+
+            string hpgl = System.IO.File.ReadAllText(@"Shuttle-HPGL.txt");
+
+            string[] hpglCommands = Regex.Split(hpgl, @"(?<=[;])");
+
+            foreach (var cmd in hpglCommands)
+            {
+                gpibSession.FormattedIO.WriteLine(cmd);
+
+                Console.WriteLine(cmd);
+            }
+        }
+    }
+}
---
+++ HPGLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HPGLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HPGLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("292b2671-c68a-4ad3-9510-8d5e3c5cc4a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
